93ac9dea784c0b8bc22b2888329c17c48ccddede
蛍 <93361361+minamikana-git@users.noreply.github.com>
2023-03-24 18:35:31 +0900

    Initial commit

---
+++ src/main/java/org/hotal/lightpvp/LightPvP.java
@@ -0,0 +1,17 @@
+package org.hotal.lightpvp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LightPvP extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
